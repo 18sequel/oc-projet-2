08f897db9353b74904a034347625b2488eb4a5f1
18sequel <18sequel@gmail.com>
2020-11-10 20:24:55 +0100

    scraper 1.1.0
    deuxième version du scraper, il est maintenant possible de scraper tous les livres d'une catégorie

--- script.py
+++ script.py
@@ -4,6 +4,40 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def get_books(category_url):
+    books_url = []
+
+    index = 1
+
+    base_url = category_url
+
+    while True:
+        if index == 1:
+            url = base_url
+        else:
+            url = base_url.replace('index', f'page-{index}')
+
+        response = requests.get(url)
+
+        if response.ok:
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            sub_books = soup.find('ol', {'class': 'row'}).findAll('li')
+
+            for book in sub_books:
+                books_url.append(book.h3.a['href'].replace('../../../', 'http://books.toscrape.com/catalogue/'))
+
+            if soup.find('li', {'class': 'next'}):
+                index += 1
+            else:
+                break
+
+        else:
+            print('get_books: erreur, "response status-code != 200"')
+
+    return books_url
+
+
 def get_book_informations(book_url):
     informations = {}
 
@@ -52,11 +86,18 @@ def get_book_informations(book_url):
         print('get_book_informations: erreur, "response status-code != 200"')
 
 
-url = input('url du livre: ')
-informations = get_book_informations(url)
+url = input('url de la catégorie: ')
 
-with open(f'{informations["title"]}.csv', 'w', encoding='utf-8') as csv_file:
+with open('category.csv', 'w', encoding='utf-8') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    csv_writer.writerow(list(informations.keys()))
-    csv_writer.writerow(list(informations.values()))
+    headers = True
+
+    for book_url in get_books(url):
+        informations = get_book_informations(book_url)
+
+        if headers:
+            csv_writer.writerow(list(informations.keys()))
+            headers = False
+
+        csv_writer.writerow(list(informations.values()))
